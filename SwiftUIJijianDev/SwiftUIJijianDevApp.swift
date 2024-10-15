b466a4e64e50e106636f6391cba889ccc4709f66
Liu <hnnk@qq.com>
2024-10-15 12:18:07 +0800

    first commit

---
+++ SwiftUIJijianDev/SwiftUIJijianDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIJijianDevApp.swift
+//  SwiftUIJijianDev
+//
+//  Created by liu on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIJijianDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
